5e3086487484d27dd5910ed78d3b3bb2fded3de6
EcixGravitycd <ly2430303@163.com>
2022-09-01 20:22:23 +0800

    1

--- t1.cpp
+++ t1.cpp
@@ -1,4 +1,4 @@
-
+/*
 //t1
 #include <iostream>
 #include <vector>
@@ -33,5 +33,37 @@ int main()
         cout << m_size;
     else
         cout << pre_m;
+    return 0;
+}
+
+*/
+
+// t2
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int val;
+    cin >> val;
+    vector<int> arr(val, 0), pre_sum(val + 1, 0), s_pre_sum(val + 1, 0);
+    for (int i = 0; i < val; ++i)
+        cin >> arr[i];
+    vector<int> s_arr(arr);
+    sort(s_arr.begin(), s_arr.end());
+    for (int i = 1; i <= val; ++i)
+    {
+        pre_sum[i] = pre_sum[i - 1] + arr[i - 1];
+        s_pre_sum[i] = s_pre_sum[i - 1] + s_arr[i - 1];
+    }
+
+    int res = 0;
+    for (int i = 0; i < val - res; ++i)
+        for (int j = i + 1; j < val; ++j)
+            if (s_pre_sum[j + 1] - s_pre_sum[i] == pre_sum[j + 1] - pre_sum[i])
+                if (*max_element(arr.begin() + i, arr.begin() + j + 1) -*min_element(arr.begin() + i, arr.begin() + j + 1) == j - i)
+                    res = max(res, j - i + 1);
+    cout << res << endl;
     return 0;
 }
