f7704f0d0ad882766151b5eec3519ce07a578a86
EcixGravitycd <ly2430303@163.com>
2022-09-01 19:23:56 +0800

    1

--- t1.cpp
+++ t1.cpp
@@ -1,20 +1,35 @@
-
-
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
 using namespace std;
-
-struct s {
-    int a : 30;
-    int b : 2;
-    char c : 7;
-};
 int main()
 {
-
-    cout << sizeof(s);
+    int cur, m_val = -1, m_size = 0, pre_m = -1, pre_m_size = 0;
+    while (cin >> cur)
+    {
+        if (m_val > cur)
+            ++m_size;
+        else
+        {
+            if (m_size > pre_m_size)
+            {
+                pre_m = m_val;
+                pre_m_size = m_size;
+            }
+            m_size = 1;
+            m_val = cur;
+        }
+    }
+    if (cur == m_val)
+    {
+        if (pre_m_size > cur)
+            cout << pre_m;
+        else
+            cout << pre_m;
+    }
+    else if (m_size > cur)
+        cout << m_size;
+    else
+        cout << pre_m;
     return 0;
 }
