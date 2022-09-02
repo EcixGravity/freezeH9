8eee1fdd5e3294dbe179efc4e5d77750b8f41131
EcixGravitycd <ly2430303@163.com>
2022-09-02 15:07:36 +0800

    1

--- t1.cpp
+++ t1.cpp
@@ -1,11 +1,12 @@
-
+/*
 // t3
 
 ListNode* reverseList(ListNode* head)
 {
 	if (head == nullptr)
 		return head;
-	ListNode* cur = pre, * pre = nullptr;
+	ListNode* cur = head;
+	ListNode* pre = nullptr;
 	while (cur->next != nullptr)
 	{
 		ListNode* n = cur->next;
@@ -15,4 +16,30 @@ ListNode* reverseList(ListNode* head)
 	}
 	cur->next = pre;
 	return cur;
+}
+*/
+
+//t2
+int findStr(string str, string mod)
+{
+	reverse(mod.begin(), mod.end());
+	int index = 0;
+	while (mod[index] == '*')
+		++index;
+	int len = str.size(), len2 = mod.size();
+
+	for (int i = index; i < len - len2; ++i)
+	{
+		if (mod[index] == str[i])
+		{
+			for (int j = index; j < len2; ++j)
+			{
+				if (mod[j] != str[i + j - index] && mod[j] != '*')
+					break;
+				if (j == len2 - 1)
+					return i - index + len2 - 1;
+			}
+		}
+	}
+	return -1;
 }
